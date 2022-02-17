57d5f4ad917aa64376a576b5c76bf5a1437fc7c0
yogesh-p-thakare3110 <95970605+yogesh-p-thakare3110@users.noreply.github.com>
2022-02-17 13:57:22 +0530

    Add files via upload

---
+++ internal/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import "os"
+
+type Config struct {
+	Environment      string
+	ConnectionString string
+}
+
+func getConfigValue(envName string, defaultValue string) string {
+	if val, ok := os.LookupEnv(envName); ok {
+		return val
+	}
+
+	return defaultValue
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Environment:      getConfigValue("EVN", "local"),
+		ConnectionString: getConfigValue("CONN_STRING", "root:India@2000@tcp(localhost:3306)/yogeshdb"),
+	}
+}
